bf573929c1fd50d940e85442815f30416cac4b1a
Taylor Cavaletto <tjcavaletto@gmail.com>
2022-02-05 13:14:00 -0500

    - Added buttons

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,8 +1,19 @@
-import React from 'react';
-import { Text, StyleSheet } from 'react-native';
+import React from "react";
+import { Text, StyleSheet, View, Button, TouchableOpacity } from "react-native";
 
 const HomeScreen = () => {
-  return <Text style={styles.text}>HomeScreen</Text>;
+  return (
+    <View>
+      <Text style={styles.text}>Hi there!</Text>
+      <Button
+        onPress={() => console.log("Button pressed")}
+        title="Go to Components Demo"
+      />
+      <TouchableOpacity onPress={() => console.log("List Pressed")}>
+        <Text>Go to List Demo</Text>
+      </TouchableOpacity>
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
